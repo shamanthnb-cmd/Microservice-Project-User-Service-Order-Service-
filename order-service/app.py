209f2b69480d99ac6938ecc2cb61d0aa2d2bb173
shamanthnb-cmd <62326303+shamanthnb-cmd@users.noreply.github.com>
2025-12-11 11:04:01 +0530

    Update app.py

--- order-service/app.py
+++ order-service/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route("/users")
+@app.route("/orders")
 def users():
-    return jsonify({"message": "User service running!"})
+    return jsonify({"message": "order service running!"})
 
 app.run(host="0.0.0.0", port=5001)
